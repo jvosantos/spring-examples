c02be6e3cd52614b0a75682c5a0294177f14bb40
Vasco Santos <jvosantos@gmail.com>
2015-12-17 14:54:13 +0000

    Configured mysql database for spring data jpa example

--- gs-accessing-data-jpa/src/main/java/hello/CustomerRepository.java
+++ gs-accessing-data-jpa/src/main/java/hello/CustomerRepository.java
@@ -6,5 +6,16 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
-    List<Customer> findByLastName(String lastName);
+  List<Customer> findByLastNameAndFirstName(String lastName, String firstName);
+
+  List<Customer> findByLastNameOrFirstName(String lastName, String firstName);
+
+  List<Customer> findByLastNameOrFirstNameContaining(String lastName, String firstName);
+
+  List<Customer> findByLastName(String lastName);
+
+  List<Customer> findByFirstName(String firstName);
+
+  List<Customer> findByLastNameContaining(String LastName);
+
 }
