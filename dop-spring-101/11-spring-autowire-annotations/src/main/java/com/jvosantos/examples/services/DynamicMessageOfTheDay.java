b07a5fcb5b14e4dcf51e4fae7c1495e6a7de50a1
Vasco Santos <jvosantos@gmail.com>
2016-01-11 10:13:31 +0000

    Added examples of autowire with annotations

---
+++ dop-spring-101/11-spring-autowire-annotations/src/main/java/com/jvosantos/examples/services/DynamicMessageOfTheDay.java
@@ -0,0 +1,37 @@
+package com.jvosantos.examples.services;
+
+import com.jvosantos.examples.models.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class DynamicMessageOfTheDay implements MessageOfTheDayService {
+  // lists
+  private List<Message> messagesValueList = new ArrayList<>();
+
+  public DynamicMessageOfTheDay() {
+  }
+
+  @Autowired
+  public void setMessagesValueList(List<Message> messagesValueList) {
+    this.messagesValueList = messagesValueList;
+  }
+
+  @Override
+  public String getMessage() {
+    // get current day
+    final int day = GregorianCalendar.getInstance().get(Calendar.DAY_OF_WEEK);
+
+    String message = "";
+
+    if (!messagesValueList.isEmpty()) {
+      message = "From list: " + messagesValueList.get(day - 1);
+    }
+
+    return message;
+  }
+}
