ab703117998b06fdb6c0b69855670b8a29e0c70c
Vasco Santos <jvosantos@gmail.com>
2016-01-11 10:12:47 +0000

    Added example with bean inheritance and configuration inheritance

---
+++ dop-spring-101/09-spring-bean-inheritance/src/main/java/com/jvosantos/examples/services/AbstractStampedMessageOfTheDay.java
@@ -0,0 +1,22 @@
+package com.jvosantos.examples.services;
+
+public abstract class AbstractStampedMessageOfTheDay implements MessageOfTheDayService {
+  private String message = "Hello World";
+
+  public AbstractStampedMessageOfTheDay() {
+    System.out.println("no-arg constructor called for " + AbstractStampedMessageOfTheDay.class.getName());
+  }
+
+  public abstract void init();
+
+  public abstract void destroy();
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
---
+++ dop-spring-101/09-spring-bean-inheritance/src/main/java/com/jvosantos/examples/services/TimeStampedMessageOfTheDay.java
@@ -0,0 +1,27 @@
+package com.jvosantos.examples.services;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class TimeStampedMessageOfTheDay extends AbstractStampedMessageOfTheDay {
+  public TimeStampedMessageOfTheDay() {
+    super();
+    System.out.println("no-arg constructor called for " + TimeStampedMessageOfTheDay.class.getName());
+  }
+
+  @Override
+  public void init() {
+    System.out.println("init() called for " + TimeStampedMessageOfTheDay.class.getName());
+  }
+
+  @Override
+  public void destroy() {
+    System.out.println("init() called for " + TimeStampedMessageOfTheDay.class.getName());
+  }
+
+  @Override
+  public String getMessage() {
+    final Date now = GregorianCalendar.getInstance().getTime();
+    return now.toString() + ": " + super.getMessage();
+  }
+}
