e236465ac261dabbe94467c82f221fb2cc06ba33
Vasco Santos <jvosantos@gmail.com>
2016-01-11 10:12:19 +0000

    Added examples with bean lifecycles

---
+++ dop-spring-101/07-spring-bean-lifecycles/src/main/java/com/jvosantos/examples/models/Message.java
@@ -0,0 +1,19 @@
+package com.jvosantos.examples.models;
+
+public class Message {
+
+  private String message = "No message";
+
+  public Message(final String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  @Override
+  public String toString() {
+    return "Message [message=" + message + "]";
+  }
+}
